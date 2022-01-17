6739d33f20477fd4dc5655e70849cce7a9f16ea8
sudo-roa <sudo.roa9393@gmail.com>
2022-01-17 16:22:25 +0900

    update BMIcalculator

--- BMIcalculator/BMIcalculator.py
+++ BMIcalculator/BMIcalculator.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 def calcBMI(weight, height):
+    height = height/100
     return weight/(height**2)
 
 def check(bmi):
@@ -26,8 +27,9 @@ root.geometry('400x200')
 label_w = tk.Label(root, text='体重')
 label_kg = tk.Label(root, text='kg')
 label_h = tk.Label(root, text='身長')
-label_m = tk.Label(root, text='m')
-message = tk.Label(root, text='体重と身長を入力してください')
+label_cm = tk.Label(root, text='cm')
+message = tk.Label(root, text='体重と身長を入力し、BMIを判定を押してください')
+
 
 entry_w = tk.Entry(width=5)
 entry_h = tk.Entry(width=5)
@@ -35,8 +37,16 @@ entry_h = tk.Entry(width=5)
 def BMIresult():
     w = float(entry_w.get())
     h = float(entry_h.get())
-    result= check(calcBMI(w,h))
-    message['text']=str(result)
+    label_input_kg =tk.Label(root, text=w)
+    label_input_cm =tk.Label(root, text=h)
+    BMI = round(calcBMI(w,h),2)
+    result= check(BMI)
+    message['text']="BMIは",BMI,"で",str(result),"です。"
+    entry_w.grid_remove()
+    entry_h.grid_remove()
+    label_input_kg.grid(column=1, row=0)
+    label_input_cm.grid(column=1, row=1)
+
 
 button = tk.Button(root, text='BMIを判定', command=BMIresult)
 
@@ -51,7 +61,7 @@ root.rowconfigure(2, weight=1)
 root.rowconfigure(3, weight=1)
 
 # 体重   [w]   kg
-# 身長   [h]   m 
+# 身長   [h]   cm 
 #     [button]
 # [    message    ]
 
@@ -61,7 +71,7 @@ label_kg.grid(column=2, row=0, sticky=tk.W)
 
 label_h.grid(column=0, row=1, sticky=tk.E)
 entry_h.grid(column=1, row=1)
-label_m.grid(column=2, row=1, sticky=tk.W)
+label_cm.grid(column=2, row=1, sticky=tk.W)
 
 #columnの指定の仕方->0から3マス分
 button.grid(column=0, row=2, columnspan=3)
