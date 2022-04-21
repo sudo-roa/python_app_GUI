4ac25e2b4a179aef4850638ac146d259c12cc4d3
sudo-roa <sudo.roa9393@gmail.com>
2022-04-21 22:58:39 +0900

    update

--- Diary/holiday.py
+++ Diary/holiday.py
@@ -3,3 +3,6 @@ import calendar, jpholiday
 
 holidays = list(map(lambda d: d[0], jpholiday.month_holidays(2021, 5)))
 print(holidays)
+
+holidays_year = jpholiday.year_holidays(2017)
+print(holidays_year)
